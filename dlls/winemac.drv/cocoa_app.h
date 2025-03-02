b0c0b912a4bc9fe59b900d21c1d4efa75398d046
Paweł Kołodziejski <aquadran@gmail.com>
2025-03-02 19:13:29 +0100

    CW HACK 10912: Mac Edit menu support

--- dlls/winemac.drv/cocoa_app.h
+++ dlls/winemac.drv/cocoa_app.h
@@ -91,6 +91,8 @@ enum {
     BOOL beenActive;
 
     NSMutableSet* windowsBeingDragged;
+    // CrossOver Hack 10912: Mac Edit menu
+    NSMutableArray* changedKeyEquivalents;
 }
 
 @property (nonatomic) CGEventSourceKeyboardType keyboardType;
@@ -129,6 +131,9 @@ enum {
 
     - (BOOL) handleEvent:(NSEvent*)anEvent;
     - (void) didSendEvent:(NSEvent*)anEvent;
+
+    // CrossOver Hack 10912: Mac Edit menu
+    - (BOOL) isEditMenuAction:(SEL)selector;
 
 @end
 
