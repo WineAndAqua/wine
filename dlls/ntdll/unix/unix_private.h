f803b2c14610e580cad20eeebd6e4557d7d2b55c
Paweł Kołodziejski <aquadran@gmail.com>
2025-03-03 21:34:33 +0100

    CW HACK 22996: write copy

--- dlls/ntdll/unix/unix_private.h
+++ dlls/ntdll/unix/unix_private.h
@@ -204,6 +204,8 @@ extern sigset_t server_block_set;
 extern pthread_mutex_t fd_cache_mutex;
 extern struct _KUSER_SHARED_DATA *user_shared_data;
 
+extern BOOL simulate_writecopy;
+
 extern void init_environment(void);
 extern void init_startup_info(void);
 extern void *create_startup_info( const UNICODE_STRING *nt_image, ULONG process_flags,
