211b2c1ab31012f1b38a957f8cf2dfe417f354fb
Paweł Kołodziejski <aquadran@gmail.com>
2025-03-04 10:57:25 +0100

    CW HACK 22434: libd3dshared

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -2313,6 +2313,15 @@ static NTSTATUS build_module( LPCWSTR load_path, const UNICODE_STRING *nt_name, 
     TRACE_(loaddll)( "Loaded %s at %p: %s\n", debugstr_w(wm->ldr.FullDllName.Buffer), *module,
                      is_builtin ? "builtin" : "native" );
 
+#if defined(__x86_64__)
+    /* CW HACK 22434 */
+    if (is_builtin == FALSE)
+    {
+	struct pe_module_loaded_params params = { *module, (void*)((BYTE*)*module + map_size) };
+	WINE_UNIX_CALL( unix_pe_module_loaded, &params );
+    }
+#endif
+
     wm->ldr.LoadCount = 1;
     *pwm = wm;
     *module = NULL;
