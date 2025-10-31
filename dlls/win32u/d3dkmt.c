d22b72a6852fd4f270b9fce6b450cf623209ec2e
Paweł Kołodziejski <aquadran@gmail.com>
2025-10-31 09:21:14 +0100

    silence NtGdiDdDDIQueryStatistics logs

--- dlls/win32u/d3dkmt.c
+++ dlls/win32u/d3dkmt.c
@@ -743,7 +743,7 @@ NTSTATUS WINAPI NtGdiDdDDIQueryAdapterInfo( D3DKMT_QUERYADAPTERINFO *desc )
  */
 NTSTATUS WINAPI NtGdiDdDDIQueryStatistics( D3DKMT_QUERYSTATISTICS *stats )
 {
-    FIXME( "(%p): stub\n", stats );
+    //FIXME( "(%p): stub\n", stats );
     return STATUS_SUCCESS;
 }
 
