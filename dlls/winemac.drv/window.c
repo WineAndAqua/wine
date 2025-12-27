487adfbb8d1c03bc0e7db2ad6cf2e6ab7036bf6d
Paweł Kołodziejski <aquadran@gmail.com>
2025-12-27 10:00:22 +0100

    DXMT support

--- dlls/winemac.drv/window.c
+++ dlls/winemac.drv/window.c
@@ -1250,6 +1250,9 @@ void macdrv_DestroyWindow(HWND hwnd)
 
     destroy_cocoa_window(data);
 
+    if (data->dxmt_client_surfaces)
+        CFRelease(data->dxmt_client_surfaces);
+
     CFDictionaryRemoveValue(win_datas, hwnd);
     release_win_data(data);
     free(data);
